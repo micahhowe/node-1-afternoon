c555d9a8f207c6c6d4dc83a8763a545464ac5d76
micahhowe <micahhowe182@gmail.com>
2019-07-12 15:18:54 -0600

    step 3 is complete and the product array is displaying

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const products = require('../products.json')
+
 const app = express()
+
 const PORT = 3002;
+
+app.get('/api/products', (request, result) => {
+    result.status(200).send(products)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
